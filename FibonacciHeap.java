b0bcfc486d93a08a236dee1d89969fa5bb9da353
Danielle Koby <daniellekoby@Danielles-MacBook-Pro.local>
2025-01-14 20:19:03 +0200

    added inner functions signatures, updated new HeapNode and FinonacciHeap(HeapNode node) constructors, insert, meld, decrease key, cut, cascadingCut, cut, addToRoots functions

--- FibonacciHeap.java
+++ FibonacciHeap.java
@@ -6,7 +6,23 @@
  */
 public class FibonacciHeap
 {
-	public HeapNode min;
+	private int size = 0;
+	private int numOfTrees = 0;
+	private int numOfLinks = 0;
+	private int numOfCuts = 0;
+
+	public HeapNode min = null;
+
+
+	/**
+	 * Inner function:
+	 * Constructor of FibonacciHeap initialized with node**/
+	public FibonacciHeap(HeapNode node){
+		FibonacciHeap fibHeap = new FibonacciHeap();
+		fibHeap.size = 1;
+		fibHeap.numOfTrees = 1;
+		fibHeap.min = node;
+	}
 	
 	/**
 	 *
@@ -21,13 +37,15 @@ public class FibonacciHeap
 	/**
 	 * 
 	 * pre: key > 0
-	 *
 	 * Insert (key,info) into the heap and return the newly generated HeapNode.
 	 *
 	 */
 	public HeapNode insert(int key, String info) 
 	{    
-		return null; // should be replaced by student code
+		HeapNode newNode = new HeapNode(key, info);
+		FibonacciHeap fibHeap = new FibonacciHeap(newNode);
+		this.meld(fibHeap);
+		return newNode;
 	}
 
 	/**
@@ -37,7 +55,7 @@ public class FibonacciHeap
 	 */
 	public HeapNode findMin()
 	{
-		return null; // should be replaced by student code
+		return min; // should be replaced by student code
 	}
 
 	/**
@@ -54,13 +72,23 @@ public class FibonacciHeap
 	/**
 	 * 
 	 * pre: 0<diff<x.key
-	 * 
 	 * Decrease the key of x by diff and fix the heap. 
 	 * 
 	 */
 	public void decreaseKey(HeapNode x, int diff) 
 	{    
-		return; // should be replaced by student code
+		assert x.key - diff > 0;
+		x.key -= diff;
+		if (x.key < this.min.key) { // Updating minimum node if needed
+			this.min = x;
+		}
+
+		// Invariant doesn't preserved
+		if ((x.parent != null) && (x.key < x.parent.key)) {
+			this.cascadingCut(x, x.parent);
+		}
+		// else - Invariant is preserved and no cuts needed
+
 	}
 
 	/**
@@ -81,7 +109,7 @@ public class FibonacciHeap
 	 */
 	public int totalLinks()
 	{
-		return 0; // should be replaced by student code
+		return this.numOfLinks; // should be replaced by student code
 	}
 
 
@@ -92,7 +120,7 @@ public class FibonacciHeap
 	 */
 	public int totalCuts()
 	{
-		return 0; // should be replaced by student code
+		return this.numOfCuts; // should be replaced by student code
 	}
 
 
@@ -103,7 +131,12 @@ public class FibonacciHeap
 	 */
 	public void meld(FibonacciHeap heap2)
 	{
-		return; // should be replaced by student code   		
+		addToRoots(heap2.min, true); // Concatenating the heap to the list of roots by the minimum node
+		this.size += heap2.size;
+		this.numOfTrees += heap2.numOfTrees;
+		if (heap2.min.key < this.min.key) {
+			this.min = heap2.min;
+		}
 	}
 
 	/**
@@ -113,7 +146,7 @@ public class FibonacciHeap
 	 */
 	public int size()
 	{
-		return 42; // should be replaced by student code
+		return this.size; // should be replaced by student code
 	}
 
 
@@ -124,8 +157,71 @@ public class FibonacciHeap
 	 */
 	public int numTrees()
 	{
-		return 0; // should be replaced by student code
+		return this.numOfTrees; // should be replaced by student code
 	}
+
+	/**
+	 Inner function-cut
+	 cut connection between two nodes
+	 */
+	public void cut(HeapNode childNode, HeapNode parentNode){
+		childNode.parent = null;
+		childNode.mark = false;
+		parentNode.rank = parentNode.rank - 1;
+		if (childNode.next == childNode){
+			parentNode.child = null;
+		}
+		else {
+			parentNode.child = childNode.next;
+			childNode.prev.next = childNode.next;
+			childNode.next.prev = childNode.prev;
+		}
+		numOfCuts++;
+		this.addToRoots(childNode, false);
+	}
+
+	/**
+	 Inner function-cascading cut
+	 */
+	public void cascadingCut(HeapNode childNode, HeapNode parentNode){
+		cut(childNode, parentNode);
+		if (parentNode.parent != null){
+			if (!parentNode.mark){
+				parentNode.mark = true;
+			}
+			else{
+				cascadingCut(parentNode, parentNode.parent);
+			}
+		}
+	}
+
+	/**
+	 Inner function-add to roots
+	 @param node - connecting this node to the list of roots
+	 @param externalHeap - if false ->  called by cut() and number of trees increases by 1
+	 if true -> called by meld and number of trees increases according to the new external heap
+	 */
+	public void addToRoots(HeapNode node, Boolean externalHeap){
+		if (this.min == null){ // adding first node to an empty heap
+			this.min = node;
+		}
+		node.next = this.min.next;
+		node.prev = this.min;
+		this.min.next = node;
+		if (!externalHeap){
+			this.numOfTrees++;
+		}
+	}
+
+
+	/**
+	  Inner function-link
+	  Link two nodes with the same rank
+	  */
+	public  void link(HeapNode node1, HeapNode node2){
+		return;
+	}
+
 
 	/**
 	 * Class implementing a node in a Fibonacci Heap.
@@ -140,5 +236,22 @@ public class FibonacciHeap
 		public HeapNode parent;
 		public int rank;
 		public boolean mark;
+
+
+		/**
+		 * constructor
+		 *
+		 */
+		public HeapNode(int key, String info){
+			this.key = key;
+			this.info = info;
+			this.child = null;
+			this.next = this;
+			this.prev = this;
+			this.parent = null;
+			this.rank = 0;
+			this.mark = false;
+
+		}
 	}
 }
